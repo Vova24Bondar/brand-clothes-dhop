06b6dac63eabde5dbce1e4a756249e133a3066ac
Vova <2vovanchik4@gmail.com>
2024-07-21 00:56:14 +0300

    try to fix all bags

--- product/decorators.py
+++ product/decorators.py
@@ -1,12 +1,10 @@
 from functools import wraps
 from django.http import JsonResponse
-from django.conf import settings
 import requests
 import json
+from django.conf import settings
 
-BOT_TOKEN = '7047933724:AAHofJokHNaWRtde2LoqokvTi8bfPBbFSWw'
-TG_BASE_URL = 'https://api.telegram.org/bot'
-
+TELEGRAM_ADMINS_ID = settings.TELEGRAM_ADMINS_ID
 
 def admin_only(view_func):
     @wraps(view_func)
@@ -14,15 +12,29 @@ def admin_only(view_func):
         data = json.loads(request.body)
         chat_id = data.get('message', {}).get('chat', {}).get('id')
         user_id = str(data.get('message', {}).get('from', {}).get('id'))
-
-        if int(user_id) in settings.TELEGRAM_ADMINS_ID:
-            return view_func(request, *args, **kwargs)
-        else:
+        try:
+            if user_id in TELEGRAM_ADMINS_ID:
+                return view_func(request, *args, **kwargs)
+            else:
+                response_data = {
+                    'chat_id': chat_id,
+                    'text': 'Access denied. You are not an administrator.'
+                }
+                requests.post(f'{settings.TG_BASE_URL}{settings.BOT_TOKEN}//sendMessage', json=response_data)
+                return JsonResponse({'error': 'Access denied'}, status=403)
+        except (json.JSONDecodeError, KeyError) as e:
             response_data = {
                 'chat_id': chat_id,
-                'text': 'Access denied. You are not an administrator.'
+                'text': 'Error processing the request. Please try again later.'
+            }
+            requests.post(f'{settings.TG_BASE_URL}{settings.BOT_TOKEN}//sendMessage', json=response_data)
+            return JsonResponse({'error': 'Bad request'}, status=400)
+        except Exception as e:
+            response_data = {
+                'chat_id': chat_id,
+                'text': 'An unexpected error occurred.'
             }
             requests.post(f'{TG_BASE_URL}{BOT_TOKEN}/sendMessage', json=response_data)
-            return JsonResponse({'error': 'Access denied'}, status=403)
+            return JsonResponse({'error': str(e)}, status=500)
 
     return _wrapped_view
